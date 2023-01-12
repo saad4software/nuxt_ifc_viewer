9f62fa5f0280bfacba87bc7df006d5022b2f3e17
saad <saad4software@gmail.com>
2023-01-12 12:23:37 +0300

    init

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# nuxt_ifc_viewer
-This code is designed to represent issue 173 in web-ifc-viewer
+# Nuxt IFC viewer
+
+this code is designed to represent [issue 173 in web-ifc-viewer](https://github.com/IFCjs/web-ifc-viewer/issues/173)
+
+## Build Setup
+
+```bash
+# install dependencies
+$ npm install
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+$ npm run start
+
+# generate static project
+$ npm run generate
+```
+
+For detailed explanation on how things work, check out the [documentation](https://nuxtjs.org).
+
---
+++ pages/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-row justify="center" align="center">
+    <div id="viewer-container" />
+  </v-row>
+</template>
+
+<script>
+import { Color } from "three";
+import { IfcViewerAPI } from "web-ifc-viewer";
+
+import { AmbientLight, AxesHelper, DirectionalLight, GridHelper, PerspectiveCamera, Scene, WebGLRenderer } from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { IFCLoader } from "web-ifc-three/IFCLoader";
+
+export default {
+
+  name: 'IndexPage',
+  data: () =>({
+
+    viewer : null,
+    ifcLoader: null,
+  }),
+  mounted() {
+
+    const container = document.getElementById("viewer-container");
+    this.viewer = new IfcViewerAPI({
+      container,
+      backgroundColor: new Color(0xffffff),
+    });
+    this.viewer.axes.setAxes();
+    this.viewer.grid.setGrid();
+    
+    this.viewer.IFC.loader = new IFCLoader();
+    this.loadIfc("models/haus.ifc");
+
+    window.onmousemove = () => this.viewer.IFC.selector.prePickIfcItem();
+
+  },
+  methods: {
+
+    async loadIfc(url) {
+      const model = await this.viewer.IFC.loadIfcUrl(url);
+      this.viewer.shadowDropper.renderShadow(model.modelID);
+    },
+
+    onChange(change){
+
+    }
+
+  }
+}
+</script>
